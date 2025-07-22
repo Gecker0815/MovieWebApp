a9325ba7b7de55156ced06c75538c00409bd1b3e
Xaradur <felixgloess@protonmail.com>
2025-07-22 19:17:08 +0200

    Set up app.py
    Create Flask application:
    - Initialize SQLAlchemy with SQLite database
    - Set up root route ('/')
    - Integrate DataManager for handling models
    - Create tables on startup using app context"

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
 from flask import Flask
+from data_manager import DataManager
+from models import db, Movie
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+data_manager = DataManager()
+
+
+@app.route('/')
+def home():
+    return "Welcome to MovieWebApp!"
+
+
+if __name__ == '__main__':
+  with app.app_context():
+    db.create_all()
+
+  app.run()
