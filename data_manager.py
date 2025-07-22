26cb2b49fd1911c4f7fa240d457f3f0a27cf114e
Xaradur <felixgloess@protonmail.com>
2025-07-22 19:13:13 +0200

    Add data_manager.py
    Add DataManager class:
    - create_user: Adds new user to database
    - get_users: Retrieves all users
    - get_movies: Fetches movies by user_id
    - add_movie: Adds new movie from dict
    - update_movie: Updates movie title by movie_id
    - delete_movie: Removes movie by movie_id

---
+++ data_manager.py
@@ -0,0 +1,26 @@
+from models import db, User, Movie
+
+class DataManager():
+    def create_user(self, name):
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+
+    def get_users(self):
+        return db.session.query(User).all()
+
+    def get_movies(self, user_id):
+        return db.session.query(Movie).filter_by(user_id=user_id).all()
+
+    def add_movie(self, movie):
+        new_movie = Movie(**movie)
+        db.session.add(new_movie)
+        db.session.commit()
+
+    def update_movie(self, movie_id, new_title):
+        db.session.query(Movie).filter(Movie.id == movie_id).update({"name": new_title})
+        db.session.commit()
+
+    def delete_movie(self, movie_id):
+        db.session.query(Movie).filter(Movie.id == movie_id).delete()
+        db.session.commit()
